43a02ba4c4022e729faeeb3f0da537ef617a2a2a
HectorSosaJr_cp <SND\HectorSosaJr_cp>
2007-08-18 02:22:01 +0000

    Test harness initial check in.

---
+++ trunk/src/TestHarness/Harness.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestHarness
+{
+    public partial class Harness : Form
+    {
+        public Harness()
+        {
+            InitializeComponent();
+        }
+    }
+}
