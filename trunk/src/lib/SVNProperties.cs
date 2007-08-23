da5248f73ddf201ec513c316c5fb7508722b2816
Hector Sosa, Jr <hectorsosajr@solarisanalytics.com>
2007-08-23 06:19:57 +0000

    SVNProperties file initial check in.

---
+++ trunk/src/lib/SVNProperties.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+
+namespace SVNManagerLib
+{
+	///<summary>
+	///</summary>
+	public class SVNProperty
+	{
+        #region Member Variables
+
+        private string _value;
+        private string _name; 
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        /// <value>The value.</value>
+        public string Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+            }
+        } 
+
+        #endregion
+    }
+
+    ///<summary>
+    ///</summary>
+    public class SVNProperties : List<SVNProperty>
+    {}
+}
