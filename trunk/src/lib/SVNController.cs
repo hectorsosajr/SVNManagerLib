d0b58b00e03fcec70328017fa48ae838b1669782
HectorSosaJr_cp <SND\HectorSosaJr_cp>
2008-04-09 05:26:24 +0000

    Added RepositoryRootDirectoryDoesNotExistException.cs to help deal with the issue of disconnected mapped drives in Vista.

--- trunk/src/lib/SVNController.cs
+++ trunk/src/lib/SVNController.cs
@@ -125,29 +125,33 @@ namespace SVNManagerLib
 		private void LoadRepositories()
 		{
 		    if ( !Equals( _serverConfiguration, null ) )
-            {
-                string repoRoot;
-                repoRoot = _serverConfiguration.RepositoryRootDirectory;
+		    {
+		        string repoRoot = _serverConfiguration.RepositoryRootDirectory;
 
-                try
+		        try
                 {
-                    DirectoryInfo rootRepoInfo;
-                    rootRepoInfo = new DirectoryInfo( repoRoot );
+                    DirectoryInfo rootRepoInfo = new DirectoryInfo( repoRoot );
 
                     // Get an array of DirectoryInfo objects. These objects represent
                     // the roots for each repository. One root directory is a repository.
-                    DirectoryInfo[] childrenRepo;
-                    childrenRepo = rootRepoInfo.GetDirectories();
-
-                    foreach ( DirectoryInfo childRepo in childrenRepo )
+                    if (rootRepoInfo.Exists)
                     {
-                        ProcessRepository( childRepo );
+                        DirectoryInfo[] childrenRepo = rootRepoInfo.GetDirectories();
+
+                        foreach (DirectoryInfo childRepo in childrenRepo)
+                        {
+                            ProcessRepository(childRepo);
+                        }
+                    }
+                    else
+                    {
+                        throw new RepositoryRootDirectoryDoesNotExistException(rootRepoInfo.Name);
                     }
                 }
                 catch ( ArgumentException )
                 {}
-            }
-        }
+		    }
+		}
 
         private void LoadRepositories( List<string> RepositoryPaths )
         {
@@ -165,11 +169,9 @@ namespace SVNManagerLib
 
 		private void ProcessRepository( DirectoryInfo childRepo )
 		{
-			DirectoryInfo[] repoDirs;
+		    DirectoryInfo[] repoDirs = childRepo.GetDirectories();
 
-			repoDirs = childRepo.GetDirectories();
-
-			foreach ( DirectoryInfo repoDir in repoDirs )
+		    foreach ( DirectoryInfo repoDir in repoDirs )
 			{
 				if ( repoDir.Name == "conf" )
 				{
@@ -178,13 +180,11 @@ namespace SVNManagerLib
 			}
 		}
 
-		private void ProcessConfigFiles( DirectoryInfo currRepoDir )
+	    private void ProcessConfigFiles( DirectoryInfo currRepoDir )
 		{
-			FileInfo[] configFiles;
+		    FileInfo[] configFiles = currRepoDir.GetFiles();
 
-			configFiles = currRepoDir.GetFiles();
-
-			foreach ( FileInfo config in configFiles )
+		    foreach ( FileInfo config in configFiles )
 			{
 				if( config.Name == "svnserve.conf" )
 				{
@@ -193,7 +193,7 @@ namespace SVNManagerLib
 			}
 		}
 
-		private void ProcessRepoConfig( FileInfo RepoConfig )
+	    private void ProcessRepoConfig( FileInfo RepoConfig )
 		{
             SVNRepository currRepo = new SVNRepository(RepoConfig.FullName, _serverConfiguration.CommandRootDirectory);
 			currRepo.Name = RepoConfig.Directory.Parent.Name;
@@ -204,4 +204,20 @@ namespace SVNManagerLib
 
 	    #endregion
 	}
+
+    /// <summary>
+    /// Exception thrown when the root directory for Subversion repositories does
+    /// not exists at run time. The most probable cause is that it was moved
+    /// since the last time SVNManagerLib was run.
+    /// </summary>
+    public class RepositoryRootDirectoryDoesNotExistException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RepositoryRootDirectoryDoesNotExistException"/> class.
+        /// </summary>
+        /// <param name="directoryPath">The missing directory path.</param>
+        public RepositoryRootDirectoryDoesNotExistException(string directoryPath) : base(directoryPath  + " has been moved or deleted.")
+        {
+        }
+    }
 }
